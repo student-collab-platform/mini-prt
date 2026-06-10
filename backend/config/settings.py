964399286bfda2204708e4d2eca07ec9f1449423
ERRADI YOUNES <youneserradi171@gmail.com>
2026-06-10 16:00:48 +0100

    Add CSRF trusted origins for collabboard

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -7,6 +7,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+CSRF_TRUSTED_ORIGINS = [
+    "https://collabboard.ddns.net",
+    "http://collabboard.ddns.net",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
